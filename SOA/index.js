9270af8bc5116a6b856e2c997906cc7b05bd0ceb
haroldc22 <103519306+haroldc22@users.noreply.github.com>
2022-04-12 10:19:08 -0500

    Add files via upload

---
+++ SOA/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   $("form").mouseover(function(){
+    $("#search").css("display","inline")
+    }).mouseout(function(){
+        $("#search").css("display","none")
+    })
+ });
