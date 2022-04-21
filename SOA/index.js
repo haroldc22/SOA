b673321287f54c171ba124ffa2f11914f0067c02
haroldc22 <103519306+haroldc22@users.noreply.github.com>
2022-04-21 06:44:20 -0500

    Add files via upload

--- SOA/index.js
+++ SOA/index.js
@@ -5,3 +5,41 @@ $(document).ready(function() {
         $("#search").css("display","none")
     })
  });
+
+class System{
+    constructor(){
+        this.search = "";
+    }
+    Search = () =>{
+        $(".btn").click(function(){
+            this.search = $("#search").val();
+            switch(this.search){
+                case "iniciar sesion":
+                case "registrarse": 
+                case "iniciar":
+                case "entrar":
+                    window.location.href = "./pages/session.html";
+                    break;
+                case "lector":
+                    window.location.href = "./pages/lector.html";
+                    break;
+                case "ir a la pagina del colegio":
+                case "mas informacion":
+                case "colegio":
+                    window.location.href = "https://www.ierepublicadehonduras.edu.co/"
+                    break;
+                case "contactanos":
+                case "contactar":
+                case "redes sociales":
+                case "whatsApp":
+                    window.location.href = "./pages/about.html" 
+                    break;
+            }
+        })   
+        
+    }
+}
+const sys = new System();
+$(document).ready(function(){
+    sys.Search();
+})
